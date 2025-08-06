fed2bbdd74edb8169867fa43997abeb4b0604292
Gururaj Hegde <rguru7825@gmail.com>
2025-08-06 18:42:00 +0530

    Update page.jsx

--- src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,7 +3,8 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-screen">
-        <SignIn fallbackRedirectUrl="/dashboard" />
+        <SignIn fallbackRedirectUrl="https://ai-pdfnotetaker.onrender.com/dashboard"
+  forceRedirectUrl="https://ai-pdfnotetaker.onrender.com/dashboard" />
     </div>
 )
 }
